14cd50d0da26d0cef3aaae88d29e4ec29726d57f
Vixterisk <vixterisk@gmail.com>
2023-12-17 02:58:04 +0500

    code cleaning

--- src/main.cpp
+++ src/main.cpp
@@ -13,28 +13,16 @@ are then transformed to fragments as inputs to fragment shader. */
 
 Matrix triangle =
 {
-   { -0.1f, -0.1f, 0.0f },
-   { 0.1f, -0.1f, 0.0f },
-   { 0.1f,  0.1f, 0.0f },
-   { -0.1f,  0.1f, 0.0f }
-};
-
-Matrix triangle2 =
-{
-   { 0.2f, 0.2f, 0.0f },
-   { 0.4f, 0.2f, 0.0f },
-   { 0.3f,  0.4f, 0.0f }
+   { -0.2f, -0.2f, 0.0f },
+   { 0.2f, -0.2f, 0.0f },
+   { 0.2f,  0.2f, 0.0f },
+   { -0.2f,  0.2f, 0.0f }
 };
 
 std::vector<GLuint> indices = 
 {
    0, 1, 3,
    1, 2, 3
-};
-
-std::vector<GLuint> indices2 =
-{
-   0, 1, 2
 };
 
 int main(int argc, char* argv[])
@@ -48,7 +36,6 @@ int main(int argc, char* argv[])
     exitWhenNull(!window, "Failed to create GLFW window.");
 
     VertexArrayData vertexArrayData = getVertexArrayData(triangle, indices);
-    VertexArrayData vertexArrayData1 = getVertexArrayData(triangle2, indices2);
     exitWhenNull(!vertexArrayData.boundVAO, "Failed to create Vertex Array Object.");
 
     std::string vertexShaderPath = getShaderAbsolutePath(GL_VERTEX_SHADER, data.vertexShader);
@@ -63,7 +50,6 @@ int main(int argc, char* argv[])
         //std::cout << glfwGetTime() << "\n";
         clearAllBuffers();
         draw(shaderProgram, *vertexArrayData.boundVAO, indices.size());
-        draw(shaderProgram, *vertexArrayData1.boundVAO, indices.size());
         /* This function processes only those events that are already in the event queue and then returns immediately. 
         Processing events will cause the window and input callbacks associated with those events to be called. */
         glfwPollEvents();
