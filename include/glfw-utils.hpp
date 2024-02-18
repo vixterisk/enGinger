72c8ac4c9d1e637be53765f7c5e9f510af530776
Vixterisk <vixterisk@gmail.com>
2024-02-18 06:03:27 +0500

    checkNotNull refactored

--- include/glfw-utils.hpp
+++ include/glfw-utils.hpp
@@ -7,21 +7,9 @@ void initGLFW();
 GLFWwindow* createWindow(const char* windowName, bool isFullscreen, bool isBorderless, int width, int height);
 
 template<class T, class F>
-T checkNotNull(T value, F errorHandler = {}, std::string errorMessage = "")
-{
-    if (&value == nullptr) {
-        errorHandler();
-        throw std::exception((const char*&)errorMessage);
-    }
-    return value;
-}
+T checkNotNull(T value, F errorHandler = {}, std::string errorMessage = "");
 
 template<class T, class F>
-T* checkNotNull(T* value, F errorHandler = {}, std::string errorMessage = "")
-{
-    if (value == nullptr) {
-        errorHandler();
-        throw std::exception((const char*&)errorMessage);
-    }
-    return value;
-}
+T* checkNotNull(T* value, F errorHandler = {}, std::string errorMessage = "");
+
+#include "check-not-null.tpp"
